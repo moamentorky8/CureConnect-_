27fa343934312f32e1464e61612e71f7ce4070e4
moamentorky8 <166957985+moamentorky8@users.noreply.github.com>
2026-04-26 20:11:44 +0300

    git commit -m "Fix: Configure Android emulator in dev.nix"

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,48 +1,21 @@
 { pkgs, ... }: {
-  # قناة النسخ المستقرة لضمان أفضل أداء
   channel = "stable-24.11";
-
-  # تعريف الأدوات المطلوبة لتشغيل فلاتر وأندرويد
   packages = [
     pkgs.flutter
     pkgs.jdk17
     pkgs.android-tools
   ];
-
-  # إعدادات البيئة
-  env = {};
-
   idx = {
-    # الإضافات البرمجية المهمة للكود
-    extensions = [
-      "Dart-Code.flutter"
-      "Dart-Code.dart"
-    ];
-
-    # تفعيل نافذة العرض (Preview) لتشغيل المحاكي السحابي
+    extensions = [ "Dart-Code.flutter" ];
     previews = {
       enable = true;
       previews = {
-        # تشغيل محاكي الأندرويد (الموبايل اللي هيظهر على اليمين)
+        # إعداد تشغيل الأندرويد
         android = {
-          command = [
-            "flutter"
-            "run"
-            "--machine"
-            "-d"
-            "android"
-            "-d"
-            "emulator-5554"
-          ];
+          command = [ "flutter" "run" "--machine" "-d" "android" "-d" "emulator-5554" ];
           manager = "flutter";
         };
       };
-    };
-
-    # الأوامر التي تنفذ عند إنشاء البيئة لأول مرة
-    onCreate = {
-      # سحب مكتبات فلاتر تلقائياً
-      install-dependencies = "cd flutter_app && flutter pub get";
     };
   };
 }
